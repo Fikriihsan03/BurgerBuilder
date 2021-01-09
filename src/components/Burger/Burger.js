2fd6d9f897f80a5e729e2ad2b67963054fb0e665
fikri <mfikriihsan03@gmail.com>
2021-01-09 18:29:57 +0700

    fix some bug

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,11 +2,22 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient";
 
 const Burger = (props) => {
+  let formattedIngredient = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  if (formattedIngredient.length === 0) {
+    formattedIngredient = <p>please start adding ingredients</p>;
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {formattedIngredient}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
