58440eca6cd4c2cc95c21da7c19071148cafaf6b
fikri <mfikriihsan03@gmail.com>
2021-01-10 19:07:40 +0700

    add order functionality

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -22,8 +22,20 @@ class BurgerBuilder extends React.Component {
         cheese: 0,
       },
       totalPrice: 4,
+      purchasable: false,
     };
   }
+  purchaseableHandler = (ingredients) => {
+    const sum = Object.keys(ingredients)
+      .map((igKey) => {
+        return ingredients[igKey];
+      })
+      .reduce((sum, el) => {
+        return sum + el;
+      }, 0);
+    this.setState({ purchasable: sum > 0 });
+  };
+
   addIngredientHandler = (type) => {
     const oldCount = this.state.ingredients[type];
     const updatedCount = oldCount + 1;
@@ -38,6 +50,7 @@ class BurgerBuilder extends React.Component {
       ingredients: updatedIngredients,
       totalPrice: updatedPrice,
     });
+    this.purchaseableHandler(updatedIngredients);
   };
   removeIngredientHandler = (type) => {
     const oldCount = this.state.ingredients[type];
@@ -56,6 +69,7 @@ class BurgerBuilder extends React.Component {
       ingredients: updatedIngredients,
       totalPrice: updatedPrice,
     });
+    this.purchaseableHandler(updatedIngredients);
   };
   render() {
     const disabledInfo = {
@@ -71,6 +85,7 @@ class BurgerBuilder extends React.Component {
           ingredientsAdded={this.addIngredientHandler}
           removeIngredients={this.removeIngredientHandler}
           disabledInfo={disabledInfo}
+          purchasable={this.state.purchasable}
           price={this.state.totalPrice}
         />
       </Aux>
